891d0f1f44c2ccee640153b0409ed254fba33713
Katerina Nefedova <Nefedova.Katia@yandex.ru>
2023-08-28 20:30:22 +0400

    все возможные комбинации

---
+++ src/main/java/com/github/Katerina163/Combination.java
@@ -0,0 +1,13 @@
+package com.github.Katerina163;
+
+public enum Combination {
+    STRAIGHT_FLUSH,
+    FOUR_OF_A_KIND,
+    FULL_HOUSE,
+    FLUSH,
+    STRAIGHT,
+    THREE_OF_A_KIND,
+    TWO_PAIRS,
+    PAIR,
+    HIGH_CARD
+}
